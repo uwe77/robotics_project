ad03d76277784132bfab2a7c11f2a0c88c4a7bfb
uwe <uwe90711@gmail.com>
2023-11-20 16:44:49 +0800

    dasd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from visual_kinematics.RobotSerial import *
+from visual_kinematics.Frame import *
 import numpy as np
 from math import pi
 from robot_model import puma560
@@ -7,20 +8,44 @@ import re
 
 def main():
     puma = puma560()
-    print("please enter the joint variable (in degree):")
+    # print("please enter the joint variable (in degree):")
+    # for i in range(6):
+    #     print(f"theta{i+1} {puma.joints[i]}", end=" ")
+    # print(":")
+    # rad = input()
+    # rad = re.split(r'\s+', rad)
+    # rad = [float(i) for i in rad]
+    # print(rad)
+    # puma.set_joint_angle(rad)
+    # puma.get_dh_trans()
+    # print("[n o a p]:\n", puma.dh_trans)
+    # psi,theta,phi = puma.get_euler()
+    # x,y,z = puma.get_pose()
+    # print(f"\noutput:\n{x} {y} {z} {psi} {theta} {phi}")
+
+
+    # matrix = []
+
+    # for i in range(4):
+    #     row = list(map(float, input("Enter row " + str(i+1) + " of the matrix, separated by spaces: ").split()))
+    #     matrix.append(row)
+
+    # Convert the list of lists to a numpy array
+    matrix = np.array([[ 0.105, -0.643, 0.759, 0.583],
+                        [ 0.702, 0.588, 0.4, 0.371],
+                        [-0.704, 0.49, 0.513, 0.204],
+                        [0., 0., 0., 1.]])
+
+    print(matrix)
+    end = Frame(matrix)
+    puma.inverse(end)
     for i in range(6):
-        print(f"theta{i+1} {puma.joints[i]}", end=" ")
-    print(":")
-    rad = input()
-    rad = re.split(r'\s+', rad)
-    rad = [float(i) for i in rad]
-    print(rad)
-    puma.set_joint_angle(rad)
-    puma.get_dh_trans()
-    print("[n o a p]:\n", puma.dh_trans)
-    psi,theta,phi = puma.get_euler()
-    x,y,z = puma.get_pose()
-    print(f"\noutput:\n{x} {y} {z} {psi} {theta} {phi}")
+        print(f"theta{i+1}:{round(puma.dh_params[i,3]*180/pi, 6)}", end=" ")
+    # print(puma.dh_params)
+    print()
+    puma.show()
+    # puma.dh_inverse_kinematics(matrix)
+
 
 if __name__ == '__main__':
     main()
--- robot_model.py
+++ robot_model.py
@@ -1,8 +1,8 @@
 from visual_kinematics.RobotSerial import*
 from visual_kinematics.RobotTrajectory import *
 from math import pi
-
-
+from sympy import sin, cos
+import sympy as sp
 class Joint:
     def __init__(self, ary=[0,0,0,0], r = 0) -> None:
         self._params = ary
@@ -31,10 +31,11 @@ class Joint:
         a = self._params[2]
         al= self._params[1]
         d = self._params[0]
-        self.dh_matrix = np.array([[c(t), -s(t)*c(a), s(t)*s(a), al*c(t)],
-                                    [s(t), c(t)*c(a), -c(t)*s(a), al*s(t)],
-                                    [0, s(a), c(a), d],
-                                    [0, 0, 0, 1]])
+        matrix = [[c(t), -s(t)*c(a), s(t)*s(a), al*c(t)],
+                [s(t), c(t)*c(a), -c(t)*s(a), al*s(t)],
+                [0, s(a), c(a), d],
+                [0, 0, 0, 1]]
+        self.dh_matrix = np.array(matrix)
 
 
 
@@ -81,12 +82,40 @@ class puma560(RobotSerial): # system ZYZ
         return self.dh_trans
     
     def dh_inverse_kinematics(self, T):
-        R = T[:3,:3]
-        theta1 = np.arctan2(R[1, 0], R[0, 0])
-        theta2 = np.arccos(R[2, 2])
-        theta3 = np.arctan2(-R[2, 1], R[2, 0])
-        theta4, theta5, theta6 = 0, 0, 0
-        return [theta1, theta2, theta3, theta4, theta5, theta6]
+        t1, t2, t3, t4, t5, t6 = sp.symbols('t1 t2 t3 t4 t5 t6')
+        A1 = sp.Matrix([[cos(t1), 0., -sin(t1), 0.],
+                        [sin(t1), 0.,  cos(t1), 0.],
+                        [0.,     -1.,       0., 0.],
+                        [0.,      0.,       0., 1.]])
+        
+        A2 = sp.Matrix([[cos(t2), -sin(t2), 0., 0.432*cos(t2)],
+                        [sin(t2),  cos(t2), 0., 0.432*sin(t2)],
+                        [0.,      0.,       1., 0.],
+                        [0.,      0.,       0., 1.]])
+        
+        A3 = sp.Matrix([[cos(t3), 0.,  sin(t3), -0.02*cos(t3)],
+                        [sin(t3), 0., -cos(t3), -0.02*sin(t3)],
+                        [0.,      1.,       0.,  0.149],
+                        [0.,      0.,       0.,  1.]])
+        
+        A4 = sp.Matrix([[cos(t4), 0., -sin(t4), 0.],
+                        [sin(t4), 0.,  cos(t4), 0.],
+                        [0.,     -1.,       0., 0.433],
+                        [0.,      0.,       0., 1.]])
+
+        A5 = sp.Matrix([[cos(t5), 0.,  sin(t5), 0.],
+                        [sin(t5), 0., -cos(t5), 0.],
+                        [0.,      1.,       0., 0.],
+                        [0.,      0.,       0., 1.]])
+
+        A6 = sp.Matrix([[cos(t6), -sin(t6), 0., 0.],
+                        [sin(t6),  cos(t6), 0., 0.],
+                        [0.,      0.,       1., 0.],
+                        [0.,      0.,       0., 1.]])
+
+        T6 = A1*A2*A3*A4*A5*A6
+        solution = sp.solve((T6 - T), (t1, t2, t3, t4, t5, t6))
+        print(solution)
 
     def get_euler(self):
         self.theta = np.arccos(self.dh_trans[2,2]) * 180/pi
